0040988a379c5ba3923abd028a0008e986272ba3
GooeyWizard <forgivenson@gmail.com>
2017-09-17 20:41:10 -0400

    Fixed the exports.

--- src/gooey-ui.js
+++ src/gooey-ui.js
@@ -3,9 +3,18 @@ import GwCheckbox from './components/gw-checkbox.vue'
 import GwMenu from './components/gw-menu.vue'
 import GwGrid from './components/gw-grid.vue'
 
+export {
+	GwInput,
+	GwCheckbox,
+	GwMenu,
+	GwGrid
+}
+
 export default {
-	GwInput: GwInput,
-	GwCheckbox: GwCheckbox,
-	GwMenu: GwMenu,
-	GwGrid: GwGrid
+	GooeyUI: {
+		GwInput: GwInput,
+		GwCheckbox: GwCheckbox,
+		GwMenu: GwMenu,
+		GwGrid: GwGrid
+	}
 }
